fc08128c732b40fdd872ff6be88cade4937530df
Julia <julia0n3@gmail.com>
2025-02-23 10:48:59 +0100

    repo init

---
+++ src/main/java/com/plateful/backend/PlatefulBackendApplication.java
@@ -0,0 +1,13 @@
+package com.plateful.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatefulBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatefulBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/plateful/backend/PlatefulBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plateful.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatefulBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
